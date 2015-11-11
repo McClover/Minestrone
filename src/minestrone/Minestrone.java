54ee65f87c15d1823fa622f334fa2028ec6310b7
Patrick <patrick.pavelcak@gmail.com>
2015-11-11 09:00:09 +0100

    Added Main class with old (but gold) hello world phrase.

---
+++ src/minestrone/Minestrone.java
@@ -0,0 +1,17 @@
+package minestrone;
+
+/**
+ * Hlavna trieda projektu, obsahuje metodu main.
+ * @author moby
+ */
+public class Minestrone{  
+    
+    /**
+     * Vstupny bod programu. Pozdravi pouzivatela starou znamou hlaskou Hello world!
+     * @param args pole parametrov pre metódu main. 
+     */
+    public static void main(String[] args){
+        System.out.println("Hello world (from NetBeans!)");
+    }
+    
+}
